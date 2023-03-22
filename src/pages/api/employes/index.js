080b5931f2c24dec8901ab34015c27a8daf0af36
Pablo Reyes <chugo940@gmail.com>
2023-03-22 14:25:36 -0600

    mas cambios

--- src/pages/api/employes/index.js
+++ src/pages/api/employes/index.js
@@ -7,15 +7,6 @@ export default async (req, res) => {
   const { method, body } = req;
 
   switch (method) {
-    case "GET":
-      try {
-        const employes = await personal.find();
-        //console.log("employes", employes);
-        res.status(200).json(employes);
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-
     case "POST":
       try {
         const newEmploye = new personal(body);
@@ -26,7 +17,13 @@ export default async (req, res) => {
         console.log(error);
       }
 
-    default:
-      return res.status(400).json({ msg: "this method is not suported" });
+    case "GET":
+      try {
+        const employes = await personal.find();
+        //console.log("employes", employes);
+        res.status(200).json(employes);
+      } catch (error) {
+        res.status(400).json({ error: error.message });
+      }
   }
 };
